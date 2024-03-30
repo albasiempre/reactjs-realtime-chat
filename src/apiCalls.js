1fec4d13ab67d774741e1323a9a256bc22e27bf6
Shohei Yamasaki <sho.yamasaki@gmail.com>
2024-03-30 23:31:42 +0900

    Fixed axiosWrapper config and removed unnecessary files

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,7 +6,10 @@ export const loginCall = async (user, dispatch) => {
   try {
     // console.log("======loginCall");
     // APIのルーティング設定を参照している
-    const response = await AxiosWrapper.create().post("api/auth/login", user);
+    const response = await AxiosWrapper.create().post(
+        "api/auth/login",
+        user
+    );
     console.log(response);
     dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
     return response;
